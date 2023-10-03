5b27963f7b17563a5cf08081f08f5f5f034556f2
JenishaR <jenisha284@gmail.com>
2023-10-04 00:14:10 +0530

    program

--- InheritanceHierarchical/Program.cs
+++ InheritanceHierarchical/Program.cs
@@ -7,11 +7,11 @@ public class Hierarchical
         ChildClass2 obj = new ChildClass2();
         Console.WriteLine("User name is "+obj.ReadUserName());
         obj.WriteName("My name is jenisha chlm");
-        //obj.Type3();
-        //obj.Type1();
+        obj.Type3();
+        obj.Type1();
         ChildClass1 obj2 = new ChildClass1();
-       // obj2.Type2();
-       // obj2.Type1();
+        obj2.Type2();
+        obj2.Type1();
         
         
     }
